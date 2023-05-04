b9df7969bc831ed9265ff1ddf0dd19c0f5818f97
Vpp2000 <victor.ponce.p@uni.pe>
2023-05-04 16:30:19 -0500

    initialized spring config server

---
+++ src/main/java/com/vpp97/azureconfigserver/AzureConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.vpp97.azureconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vpp97/azureconfigserver/AzureConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpp97.azureconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
